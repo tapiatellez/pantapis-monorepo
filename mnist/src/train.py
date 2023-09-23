bb47cb38388679872bbb43a7121c6e55d3234914
tapiatellez <tapiatellez@gmail.com>
2023-09-23 16:29:16 -0600

    Setup pre-commit hooks with pants

--- mnist/src/train.py
+++ mnist/src/train.py
@@ -1,5 +1,3 @@
-from pathlib import Path
-
 import torch
 import torch.nn as nn
 import torch.optim as optim
@@ -12,7 +10,6 @@ from mnist.src.config import Config
 
 
 if __name__ == "__main__":
-
     # Set device configuration
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
